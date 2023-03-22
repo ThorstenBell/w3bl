21d22933079bf388f1f3a0c75f19c0422c36b57c
ThorstenBell <thorst@gmail.com>
2023-03-22 21:41:14 +0100

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ToolbarComponent from "@/components/ToolbarComponent.vue";
+import {RouterView} from "vue-router";
+</script>
+
+<template>
+  <toolbar-component></toolbar-component>
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ToolbarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <img alt="Thorsten" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<style scoped>
+nav {
+  position: fixed;
+  height: 200px;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background: var(--vt-c-white);
+}
+</style>
