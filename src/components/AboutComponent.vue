17daf8644957aeabf650689d87597d8cf343bee3
Thorsten <thorst@gmail.com>
2025-05-24 17:41:31 +0200

    GH Action deploy to gh pages

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -1,28 +1,33 @@
-
 <template>
   <section id="about">
     <div>
       <h2>About Me</h2>
-      <p>As a developer, I specialise in creating intuitive and engaging user experiences for web and
-        mobile applications. With a strong background in HTML, CSS, and JavaScript, I am adept at developing
-        responsive, cross-browser compatible frontend interfaces. I also have experience with Python, R,
-        PHP, and SQL. I currently work as a developer and UX Designer in a software development team that
-        produces SaaS products for marketing professionals, as well as undertaking a masters in Computer Science
-        with Data analytics in my spare time.
+      <p>I'm a front end developer with a passion for crafting websites and software that are not only visually
+        appealing but also highly functional and accessible. I thrive on leveraging the latest technologies to bring
+        innovative ideas to life, ensuring every project is responsive and user-friendly across all devices.
       </p>
-      <p>I am highly organised and detail-oriented, with a strong emphasis on delivering high-quality,
-        bug-free code. I am also a team player and enjoy collaborating with other developers, designers and
-        project managers to bring projects to completion. As well as, understanding and empathising with
-        users to develop highly functional digital products.
+      <p>With a keen eye for design and a strong foundation in modern development practices, I specialize in creating
+        seamless digital experiences that meet the highest standards of performance and accessibility. My commitment to
+        continuous learning and staying updated with industry trends allows me to deliver cutting-edge solutions that
+        exceed expectations.
       </p>
     </div>
     <div>
       <h2>Software Skills</h2>
-      <h3><svg-icon type="mdi" :path="mdiCodeTags"></svg-icon> Development</h3>
+      <h3>
+        <svg-icon type="mdi" :path="mdiCodeTags"></svg-icon>
+        Development
+      </h3>
       <p>HTML, CSS (Sass), JavaScript (Vue, jQuery), PHP (Laravel), SQL</p>
-      <h3><svg-icon type="mdi" :path="mdiPalette "></svg-icon> Design</h3>
+      <h3>
+        <svg-icon type="mdi" :path="mdiPalette "></svg-icon>
+        Design
+      </h3>
       <p>Adobe CC: InDesign, Illustrator, Photoshop, XD</p>
-      <h3><svg-icon type="mdi" :path="mdiChartArc "></svg-icon> Data Analytics</h3>
+      <h3>
+        <svg-icon type="mdi" :path="mdiChartArc "></svg-icon>
+        Data Analytics
+      </h3>
       <p>Python, R, SQL</p>
     </div>
   </section>
@@ -31,7 +36,7 @@
 <script setup lang="ts">
 import SvgIcon from '@jamescoyle/vue-icon';
 
-import { mdiCodeTags, mdiPalette, mdiChartArc  } from '@mdi/js';
+import {mdiCodeTags, mdiPalette, mdiChartArc} from '@mdi/js';
 
 </script>
 
